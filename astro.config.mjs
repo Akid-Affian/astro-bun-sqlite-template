3392138f4618889df0543cea095e592cf0c89d14
AkidAffian <denniskidman88@gmail.com>
2024-09-19 16:44:39 +0800

    Update astro.config.mjs, package.json, index.astro, Layout.astro, Navbar.astro, um.astro, umcced.astro, and tailwind.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import bun from "@nurodev/astro-bun";
+import alpine from '@astrojs/alpinejs';
 
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   adapter: bun(),
   output: 'hybrid',
-  integrations: [tailwind()],
+  integrations: [tailwind(),
+  alpine()
+  ],
 });
