da2a9c9ed994341bf7e38fb878704f5a175124d7
AkidAffian <denniskidman88@gmail.com>
2024-09-25 15:15:02 +0800

    delete the whole thing

--- src/types/Props.ts
+++
@@ -1,8 +0,0 @@
-export interface Props {
-    name: string;        
-    size?: number;       
-    class?: string;  
-    fill?: string;       
-    stroke?: string;  
-    rotation?: number;   
-  }
